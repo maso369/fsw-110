73f322f6200522242a9dd5de2ab2094bcd20a0f0
maso369 <60929713+maso369@users.noreply.github.com>
2020-04-30 20:56:18 -0400

    Add files via upload

---
+++ week2/index2.js
@@ -0,0 +1,32 @@
+var newNav = document.createElement("navbar")
+newNav.textContent = "home about help"
+document.body.append(newNav)
+
+var newH1 = document.createElement("h1")
+newH1.textContent = "WALLYWORLD"
+document.body.append(newH1)
+
+var newP = document.createElement("p")
+newP.textContent = "WELCOME TO WALLYWORLD"
+document.body.append(newP)
+
+var newLi = document.createElement("li")
+newLi.textContent = "WALLY"
+document.body.append(newLi)
+
+var newLi = document.createElement("li")
+newLi.textContent = "WORLD"
+document.body.append(newLi)
+
+var newLi = document.createElement("li")
+newLi.textContent = "IS"
+document.body.append(newLi)
+
+var newLi = document.createElement("li")
+newLi.textContent = "FUN"
+document.body.append(newLi)
+
+
+var newFooter = document.createElement("footer")
+newFooter.textContent = "WALLY WORLD IS A VERY FUN PLACE"
+document.body.append(newFooter)
